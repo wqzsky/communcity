a53f70386828180ba1d15df0fc891eb440404b22
wqzsky <1421262066@qq.com>
2020-07-18 23:33:15 +0800

    Initial commit

---
+++ src/main/java/com/muo/commuciate/comm/CommApplication.java
@@ -0,0 +1,13 @@
+package com.muo.commuciate.comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/muo/commuciate/comm/controller/helloController.java
@@ -0,0 +1,15 @@
+package com.muo.commuciate.comm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class helloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
