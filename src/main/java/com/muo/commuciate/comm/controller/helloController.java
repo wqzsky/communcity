24a14bbd3824b341d60df545849a45aeb4c475fb
wqzsky <1421262066@qq.com>
2020-07-21 21:46:57 +0800

    2020年7月21日导航栏完成

--- src/main/java/com/muo/commuciate/comm/controller/helloController.java
+++ src/main/java/com/muo/commuciate/comm/controller/helloController.java
@@ -1,15 +1,12 @@
 package com.muo.commuciate.comm.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
