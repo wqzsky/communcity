e65b66d0b7a19bafb1f1b450e253f3d628421d8a
wqzsky <1421262066@qq.com>
2020-07-27 23:09:51 +0800

    2020年7月27日

---
+++ src/main/java/com/muo/commuciate/comm/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.muo.commuciate.comm.mapper;
+
+import com.muo.commuciate.comm.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+}
