b00b905aa47d7fe4d8c63803559ccf533a404a1c
wqzsky <1421262066@qq.com>
2020-08-01 13:03:37 +0800

    2020年8月1日-分页完成

--- src/main/java/com/muo/commuciate/comm/controller/IndexController.java
+++ src/main/java/com/muo/commuciate/comm/controller/IndexController.java
@@ -1,39 +1,39 @@
 package com.muo.commuciate.comm.controller;
 
-import com.muo.commuciate.comm.dto.QuestionDTO;
-import com.muo.commuciate.comm.mapper.QuestionMapper;
+import com.muo.commuciate.comm.dto.PaginationDTO;
 import com.muo.commuciate.comm.mapper.UserMapper;
-import com.muo.commuciate.comm.model.Question;
 import com.muo.commuciate.comm.model.User;
 import com.muo.commuciate.comm.service.QuestionService;
-import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.jws.WebParam;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
-    private  UserMapper userMapper;
+    private UserMapper userMapper;
     @Autowired
     private QuestionService questionService;
+
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size
+    ) {
         Cookie[] cookies = request.getCookies();
-        if(cookies != null){
+        if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
-                if(cookie.getName().equals("token")){
+                if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
                     User user = userMapper.findByToken(token);
-                    if(user != null){
-                        request.getSession().setAttribute("user",user);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
                         //try...catch快捷键 ctrl+alt+t
                     }
                     break;
@@ -41,8 +41,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/com/muo/commuciate/comm/service/QuestionService.java
+++ src/main/java/com/muo/commuciate/comm/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.muo.commuciate.comm.service;
 
+import com.muo.commuciate.comm.dto.PaginationDTO;
 import com.muo.commuciate.comm.dto.QuestionDTO;
 import com.muo.commuciate.comm.mapper.QuestionMapper;
 import com.muo.commuciate.comm.mapper.UserMapper;
@@ -19,17 +20,28 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
-           /* questionDTO.setId(question.getId());*/
+            /* questionDTO.setId(question.getId());*/
             BeanUtils.copyProperties(question, questionDTO);
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
 }
