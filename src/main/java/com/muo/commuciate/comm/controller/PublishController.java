0da8daefe736dba6862d9df6e6b2e90f2642452f
wqzsky <1421262066@qq.com>
2020-08-03 21:30:45 +0800

    2020年8月3日 p29 拦截器finish，

--- src/main/java/com/muo/commuciate/comm/controller/PublishController.java
+++ src/main/java/com/muo/commuciate/comm/controller/PublishController.java
@@ -1,7 +1,6 @@
 package com.muo.commuciate.comm.controller;
 
 import com.muo.commuciate.comm.mapper.QuestionMapper;
-import com.muo.commuciate.comm.mapper.UserMapper;
 import com.muo.commuciate.comm.model.Question;
 import com.muo.commuciate.comm.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired
     private QuestionMapper questionMapper;
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -49,20 +45,7 @@ public class PublishController {
             model.addAttribute("error", "标签不能为空");
             return "publish";
         }
-        Cookie[] cookies = request.getCookies();
-        User user = null;
-        if(cookies != null && cookies.length != 0)
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                String token = cookie.getValue();
-                user = userMapper.findByToken(token);
-                if (user != null) {
-                    request.getSession().setAttribute("user", user);
-                    //try...catch快捷键 ctrl+alt+t
-                }
-                break;
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             model.addAttribute("error", "用户未登录");
             return "publish";
