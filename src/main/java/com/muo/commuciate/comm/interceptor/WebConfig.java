af83bb28b228e5e4036fe6f1dab668e24667c877
wqzsky <1421262066@qq.com>
2020-08-03 23:57:24 +0800

    2020年8月3日 p29 拦截器finish，

--- src/main/java/com/muo/commuciate/comm/interceptor/WebConfig.java
+++ src/main/java/com/muo/commuciate/comm/interceptor/WebConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
+
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private SessionInterceptor sessionInterceptor;
